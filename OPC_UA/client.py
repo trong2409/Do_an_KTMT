b08dd433ca8eecfd51415710e5dde0017cfa29c8
Nam Diep <namdiep239@gmail.com>
2022-10-15 17:05:43 +0700

    upload code and measure result

--- OPC_UA/client.py
+++ OPC_UA/client.py
@@ -10,7 +10,7 @@ hostname=socket.gethostname()
 IPAddr=socket.gethostbyname(hostname)
 
 # url = "opc.tcp://192.168.56.1:4840"
-url = "opc.tcp://"+IPAddr+":4840" 
+url = "opc.tcp://192.168.123.102:4840"
 
 client = Client(url)
 
@@ -18,17 +18,20 @@ client.connect()
 print('Client connected')
 print('==========================================')
 
-count = 200
+count = 1000
 result = {"2b":[]}
 
 '''Because printing affect the speed of the program, I'll comment them out.'''
 
 DataNode = client.get_node("ns=2;i=2")
-
+# lendata = False
 while (count>0):
     req = time.time()
     # print("Request: "+ datetime.datetime.now().strftime("%H:%M:%S.%f"))
     Data = DataNode.get_value()
+    # if not lendata:
+    #     print(len(Data))
+    #     lendata = True
     # print("Temperature: ",Temperature)
     res = time.time()
     # print("Respone: "+ datetime.datetime.now().strftime("%H:%M:%S.%f"))
@@ -48,3 +51,5 @@ for col, data in enumerate(array):
     worksheet.write_column(row, col, data)
 workbook.close()
 print('done')
+
+client.disconnect()
