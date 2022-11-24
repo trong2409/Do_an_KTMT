60a3923fd649d33d027ac6bb9a50f872c96e1dc3
trong2409 <nvtrong249@gmail.com>
2022-11-24 09:27:24 +0700

    delete some OCPUA

--- Python_Station/server_05/download_driver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo systemctl stop server_opc
-sleep 1
-sudo systemctl disable server_opc
-sudo avrdude -C avrdude.conf -v -p atmega2560 -c wiring -P /dev/ttyACM0 -b 115200 -D -U flash:w:DRIVER_PAUL_STATION_05.ino.hex:i
-sleep 1
-sudo systemctl enable server_opc
-sleep 1
-sudo systemctl start server_opc
