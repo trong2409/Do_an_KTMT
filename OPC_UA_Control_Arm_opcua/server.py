1e6e463115879d18cc22171a68a83fd39114cff0
Kuzuma-kun <ngoccatt@gmail.com>
2023-02-03 14:26:47 +0700

    add more nodes

--- OPC_UA_Control_Arm_opcua/server.py
+++ OPC_UA_Control_Arm_opcua/server.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3
+
 import logging
-import socket
+import netifaces as ni
 import asyncio
+import traceback
 
-
-
-# import rospy
-# import hiwonder
-# from std_msgs.msg import UInt8, UInt16, Float32, Bool
-# from std_srvs.srv import Empty
-# from opcua.msg import JetMax as JetMaxState
+import rospy
+#from std_msgs.msg import UInt8, UInt16, Float32, Bool
+#from std_srvs.srv import Empty
+from opc_ros.msg import SetJetMax, JetMax
 
 from opcua import ua, uamethod, Server
 
 _logger = logging.getLogger(__name__)
 
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
+IPAddr = ni.ifaddresses('ap0')[ni.AF_INET][0]['addr']
 
 MIN_MIDSERVO = 0
-MAX_MIDSERVO = 270
+MAX_MIDSERVO = 240
 
-MIN_LEFTSERVO = 0
-MAX_LEFTSERVO = 180
+MIN_LEFTSERVO = -30
+MAX_LEFTSERVO = 210
 
-MIN_RIGHTSERVO = 0
-MAX_RIGHTSERVO = 90
+MIN_RIGHTSERVO = -120
+MAX_RIGHTSERVO = 120
+
+MIN_ROTATOR = 0
+MAX_ROTATOR = 180
 
 MIN_GRIPPER = 0
 MAX_GRIPPER = 180
@@ -33,30 +34,43 @@ MAX_GRIPPER = 180
 midServo = 90
 leftServo = 90
 rightServo = 90
+rotatorServo = 90
 gripperServo = 90
+suckcup = False
 speed = 5
+
+JetMaxControlList = list()
 
 
 @uamethod
 def moveServo(parent, servo_name: str, angle_decrease: bool):
-    global midServo, leftServo, rightServo, gripperServo, speed
+    global midServo, leftServo, rightServo, rotatorServo, speed
     # print(f'{servo_name}, {angle_decrease}')
-    if servo_name == "M":
+    if servo_name == "mid":
         if angle_decrease and midServo > MIN_MIDSERVO:
-            midServo = midServo - speed if (midServo - speed >= MIN_MIDSERVO) else MIN_MIDSERVO
+            midServotemp = midServo - speed if (midServo - speed >= MIN_MIDSERVO) else MIN_MIDSERVO
         elif not angle_decrease and midServo < MAX_MIDSERVO:
             midServo = midServo + speed if (midServo + speed <= MAX_MIDSERVO) else MAX_MIDSERVO
-    if servo_name == "L":
+            try:
+                jetmax_pub.publish(SetJetMax(x=-10,y=0,z=0,duration=0.1))
+            except Exception as e:
+                traceback.print_exc()
+    if servo_name == "left":
         if angle_decrease and leftServo > MIN_LEFTSERVO:
             leftServo = leftServo - speed if (leftServo - speed >= MIN_LEFTSERVO) else MIN_LEFTSERVO
         elif not angle_decrease and leftServo < MAX_LEFTSERVO:
             leftServo = leftServo + speed if (leftServo + speed <= MAX_LEFTSERVO) else MAX_LEFTSERVO
-    if servo_name == "R":
+    if servo_name == "right":
         if angle_decrease and rightServo > MIN_RIGHTSERVO:
             rightServo = rightServo - speed if (rightServo - speed >= MIN_RIGHTSERVO) else MIN_RIGHTSERVO
         elif not angle_decrease and rightServo < MAX_RIGHTSERVO:
             rightServo = rightServo + speed if (rightServo + speed <= MAX_RIGHTSERVO) else MAX_RIGHTSERVO
-    if servo_name == "G":
+    if servo_name == "rotator":
+        if angle_decrease and rotatorServo > MIN_ROTATOR:
+            rotatorServo = rotatorServo - speed if (rotatorServo - speed >= MIN_ROTATOR) else MIN_ROTATOR
+        elif not angle_decrease and rotatorServo < MAX_ROTATOR:
+            rotatorServo = rotatorServo + speed if (rotatorServo + speed <= MAX_ROTATOR) else MAX_ROTATOR
+    if servo_name == "gripper":
         if angle_decrease and gripperServo > MIN_GRIPPER:
             gripperServo = gripperServo - speed if (gripperServo - speed >= MIN_GRIPPER) else MIN_GRIPPER
         elif not angle_decrease and gripperServo < MAX_GRIPPER:
@@ -68,27 +82,26 @@ def autoServo(parent, servo_name: str, direction: int):
     global SERVO_LOCK_AUTORUN
     if direction < -1 or direction > 1:
         return "NOTOK"
-    if servo_name == "M":
+    if servo_name == "mid":
         SERVO_LOCK_AUTORUN["MID_SERVO"] = direction
-    if servo_name == "L":
+    if servo_name == "left":
         SERVO_LOCK_AUTORUN["LEFT_SERVO"] = direction
-    if servo_name == "R":
+    if servo_name == "right":
         SERVO_LOCK_AUTORUN["RIGHT_SERVO"] = direction
-    if servo_name == "G":
+    if servo_name == "rotator":
+        SERVO_LOCK_AUTORUN["ROTATOR_SERVO"] = direction
+    if servo_name == "gripper":
         SERVO_LOCK_AUTORUN["GRIPPER_SERVO"] = direction
     return "OK"
-
-
-
 
 # -1: decrease angle, 0: stop, 1: increase angle
 SERVO_LOCK_AUTORUN = {
     "MID_SERVO": 0,
     "LEFT_SERVO": 0,
     "RIGHT_SERVO": 0,
+    "ROTATOR_SERVO": 0,
     "GRIPPER_SERVO": 0
 }
-
 
 class SubHandler(object):
     """
@@ -101,27 +114,36 @@ class SubHandler(object):
     def event_notification(self, event):
         _logger.warning("Python: New event %s", event)
 
-
-
 #replace server.start()
 async def main():
-    async def updateRobot():
+    def updateRobot(data):
         """
         function to read Robot Arm angle data and update to opcua object model (opcua server)
         """
-        global midServo, leftServo, rightServo, gripperServo, speed
-        while True:
+        global midServo, leftServo, rightServo, rotatorServo, speed, suckcup
+        midServo = data.joint1
+        leftServo = data.joint2
+        rightServo = data.joint3
+        rotatorServo = data.pwm1
+        gripperServo = data.pwm2
+        suckcup = data.sucker
+        try:
             if midServo != midServoNode.get_value():
                 midServoNode.set_value(midServo)
             if leftServo != leftServoNode.get_value():
                 leftServoNode.set_value(leftServo)
             if rightServo != rightServoNode.get_value():
                 rightServoNode.set_value(rightServo)
+            if rotatorServo != rotatorServoNode.get_value():
+                rotatorServoNode.set_value(rotatorServo)
+            if suckcup != suckNode.get_value():
+                suckNode.set_value(suckcup)
             if gripperServo != gripperServoNode.get_value():
                 gripperServoNode.set_value(gripperServo)
             if speed != speedNode.get_value():
                 speed = speedNode.get_value()
-            await asyncio.sleep(0.005)
+        except Exception as e:
+            traceback.print_exc()
 
     async def autoRun():
         """
@@ -132,31 +154,52 @@ async def main():
         """
         global SERVO_LOCK_AUTORUN
         while True:
-            if SERVO_LOCK_AUTORUN["LEFT_SERVO"] == -1:
-                moveServo(idx, "L", ua.Variant(True, ua.VariantType.Boolean))
-            if SERVO_LOCK_AUTORUN["LEFT_SERVO"] == 1:
-                moveServo(idx, "L", ua.Variant(False, ua.VariantType.Boolean))
-            if SERVO_LOCK_AUTORUN["RIGHT_SERVO"] == -1:
-                moveServo(idx, "R", ua.Variant(True, ua.VariantType.Boolean))
-            if SERVO_LOCK_AUTORUN["RIGHT_SERVO"] == 1:
-                moveServo(idx, "R", ua.Variant(False, ua.VariantType.Boolean))
-            if SERVO_LOCK_AUTORUN["MID_SERVO"] == -1:
-                moveServo(idx, "M", ua.Variant(True, ua.VariantType.Boolean))
-            if SERVO_LOCK_AUTORUN["MID_SERVO"] == 1:
-                moveServo(idx, "M", ua.Variant(False, ua.VariantType.Boolean))
-            if SERVO_LOCK_AUTORUN["GRIPPER_SERVO"] == -1:
-                moveServo(idx, "G", ua.Variant(True, ua.VariantType.Boolean))
-            if SERVO_LOCK_AUTORUN["GRIPPER_SERVO"] == 1:
-                moveServo(idx, "G", ua.Variant(False, ua.VariantType.Boolean))
-            await asyncio.sleep(0.1)
-            # print("sleep 5 seconds")
+            try:
+                if SERVO_LOCK_AUTORUN["LEFT_SERVO"] == -1:
+                    moveServo(idx, "left", ua.Variant(True, ua.VariantType.Boolean))
+                if SERVO_LOCK_AUTORUN["LEFT_SERVO"] == 1:
+                    moveServo(idx, "left", ua.Variant(False, ua.VariantType.Boolean))
+                if SERVO_LOCK_AUTORUN["RIGHT_SERVO"] == -1:
+                    moveServo(idx, "right", ua.Variant(True, ua.VariantType.Boolean))
+                if SERVO_LOCK_AUTORUN["RIGHT_SERVO"] == 1:
+                    moveServo(idx, "right", ua.Variant(False, ua.VariantType.Boolean))
+                if SERVO_LOCK_AUTORUN["MID_SERVO"] == -1:
+                    moveServo(idx, "mid", ua.Variant(True, ua.VariantType.Boolean))
+                if SERVO_LOCK_AUTORUN["MID_SERVO"] == 1:
+                    moveServo(idx, "mid", ua.Variant(False, ua.VariantType.Boolean))
+                if SERVO_LOCK_AUTORUN["ROTATOR_SERVO"] == -1:
+                    moveServo(idx, "rotator", ua.Variant(True, ua.VariantType.Boolean))
+                if SERVO_LOCK_AUTORUN["ROTATOR_SERVO"] == 1:
+                    moveServo(idx, "rotator", ua.Variant(False, ua.VariantType.Boolean))
+                if SERVO_LOCK_AUTORUN["GRIPPER_SERVO"] == -1:
+                    moveServo(idx, "gripper", ua.Variant(True, ua.VariantType.Boolean))
+                if SERVO_LOCK_AUTORUN["GRIPPER_SERVO"] == 1:
+                    moveServo(idx, "gripper", ua.Variant(False, ua.VariantType.Boolean))
+                await asyncio.sleep(0.2)
+                # print("sleep 5 seconds")
+            except KeyboardInterrupt:
+                break
+ 
 
-
-
-
-    server = Server()
+    async def ros_run():
+        global JetMaxControlList
+        rospy.init_node("opcua", anonymous=True)
+        jetmax_state = rospy.Subscriber('/jetmax/status',JetMax,callback=updateRobot)
+        # jetmax_pub = rospy.Publisher('/jetmax/relative_command',SetJetMax,queue_size=1)
+        # JetMaxControlList.append(jetmax_state)
+        while not rospy.is_shutdown():
+            try:
+                #jetmax_pub.publish(SetJetMax(x=-10,y=0,z=0,duration=0.5))
+                await asyncio.sleep(0.5)
+            except ros.ROSInterruptException:
+                pass
+            except KeyboardInterrupt:
+                break
+            
+    
 
     # server.init()
+    server = Server()
 
     # endpoint: address to connect to
     server.set_endpoint(f"opc.tcp://{IPAddr}:4841")
@@ -177,10 +220,12 @@ async def main():
 
     # add some nodes
     myfolder = server.nodes.objects.add_folder(idx, "Robot Arm")
-    midServoNode = myfolder.add_variable(idx, "M", 90)
-    leftServoNode = myfolder.add_variable(idx, "L", 90)
-    rightServoNode = myfolder.add_variable(idx, "R", 90)
-    gripperServoNode = myfolder.add_variable(idx, "G", 90)
+    midServoNode = myfolder.add_variable(idx, "mid", 90, varianttype=ua.VariantType.Double)
+    leftServoNode = myfolder.add_variable(idx, "left", 90, varianttype=ua.VariantType.Double)
+    rightServoNode = myfolder.add_variable(idx, "right", 90, varianttype=ua.VariantType.Double)
+    rotatorServoNode = myfolder.add_variable(idx, "rotator", 90, varianttype=ua.VariantType.Int16)
+    gripperServoNode = myfolder.add_variable(idx, "gripper", 90, varianttype=ua.VariantType.Int16)
+    suckNode = myfolder.add_variable(idx, "suckcup", False, varianttype=ua.VariantType.Boolean)
     speedNode = myfolder.add_variable(idx, "S", 5)
 
     inargx = ua.Argument()
@@ -236,23 +281,35 @@ async def main():
     midServoNode.set_writable()
     leftServoNode.set_writable()
     rightServoNode.set_writable()
-    gripperServoNode.set_writable()
+    rotatorServoNode.set_writable()
     speedNode.set_writable()
     
-    # rospy.init_node("opcua_control", anonymous=True)
-    # jetmax_sub = rospy.Subscriber("/jetmax/status", JetMaxState, 
-    #                               queue_size=1,
-    #                               callback=lambda msg: print(msg))
+    
 
     with server:
         print(f'OPC server running at {server.endpoint[0]}://{server.endpoint[1]}')
         # asyncio.gather giup chay 2 ham async 1 cach "song song"
-        await asyncio.gather(updateRobot(), autoRun())
+        
+        await asyncio.gather(autoRun(), ros_run())
+        #await asyncio.gather(autoRun(), return_exceptions=True)
+        #await asyncio.gather(ros_run())
 
 
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    
+    try:
+        logging.basicConfig(level=logging.INFO)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+    except rospy.ROSInterruptException:
+        loop.close()
+    except KeyboardInterrupt:
+        print("Interrupt")
+        
+    finally:
+        loop.close()
+        exit()
 
-    asyncio.run(main())
+
